ab9e4f45d7deff4c5aec7453bc2f365c74259209
Dousea <orlando.timotius.hutapea@gmail.com>
2020-06-18 21:54:19 +0700

    Added rescue on access denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name address phone_number])
   end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { render nothing: true, status: :forbidden }
+      format.html { redirect_to main_app.root_url, alert: exception.message }
+      format.js   { render nothing: true, status: :forbidden }
+    end
+  end
 end
