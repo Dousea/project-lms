45061ceb52eb44f78d943ba0a70b2a4477d80bde
Dousea <orlando.timotius.hutapea@gmail.com>
2020-06-18 22:35:46 +0700

    Added abilities for books

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,6 +6,8 @@ class Ability
   def initialize(user)
     return unless user.present?
 
+    can :read, Book if user.Member?
+    can :manage, Book if user.Moderator? || user.Admin?
     can :manage, User if user.Admin?
   end
 end
