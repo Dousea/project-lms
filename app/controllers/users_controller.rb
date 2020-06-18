1caf717a7f22f64f57435750e9fd11f9c7a1990b
Dousea <orlando.timotius.hutapea@gmail.com>
2020-06-18 21:55:39 +0700

    Added authorization and redirection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  authorize_resource
+
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => e
+    redirect_to users_path, alert: e.message
   end
 end
