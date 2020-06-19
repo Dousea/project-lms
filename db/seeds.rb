506af7aeb074fe75dabbcdb40216601dea51bc02
Dousea <orlando.timotius.hutapea@gmail.com>
2020-06-19 11:07:18 +0700

    Added books seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+require 'csv'
+
+def find_or_create_author(book, author_name)
+  return if book.authors.exists?(name: author_name)
+
+  author = Author.where(name: author_name).first
+
+  if author.nil?
+    book.authors.build(name: author_name)
+  else
+    book.authors << author
+  end
+end
+
+def find_or_create_subject(book, subject_name)
+  return if book.subjects.exists?(name: subject_name)
+
+  subject = Subject.where(name: subject_name).first
+
+  if subject.nil?
+    book.subjects.build(name: subject_name)
+  else
+    book.subjects << subject
+  end
+end
+
+def create_book_from_row(row)
+  book = Book.new
+  book.title = row['title']
+  book.published_at = Time.parse(row['published_at']).to_date
+  book.publisher = Publisher.where(name: row['publisher'])
+                            .first_or_create
+
+  find_or_create_author(book, row['author'])
+  row['subjects'].split('|') { |subject| find_or_create_subject(book, subject) }
+
+  book.save
+  book
+end
+
+def seed_books
+  csv_text = File.read(Rails.root.join('lib', 'seeds', 'books_mock_data.csv'))
+  csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
+  csv.each do |row|
+    book = create_book_from_row(row)
+
+    if book.persisted?
+      puts "#{book.title} saved"
+    else
+      puts "Errors: #{book.errors.messages}"
+    end
+  end
+end
