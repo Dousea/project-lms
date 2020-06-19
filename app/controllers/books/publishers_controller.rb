4f139ca37628f8f5a0f170096f8dc6387e84ef65
Dousea <orlando.timotius.hutapea@gmail.com>
2020-06-19 11:47:26 +0700

    Added `show` `index` actions

--- app/controllers/books/publishers_controller.rb
+++ app/controllers/books/publishers_controller.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
 class Books::PublishersController < ApplicationController
+  authorize_resource
+
+  def index
+    params[:page] = 1 unless params.key? :page
+    @publishers = Publisher.page(params[:page])
+  end
+
+  def show
+    @publisher = Publisher.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => e
+    redirect_to books_publishers_path, alert: e.message
+  end
 end
