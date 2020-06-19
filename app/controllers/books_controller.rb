03fe4e67a544c3c915113b41d7347239e0122836
Dousea <orlando.timotius.hutapea@gmail.com>
2020-06-19 11:22:00 +0700

    Added `show` action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,4 +7,10 @@ class BooksController < ApplicationController
     params[:page] = 1 unless params.key? :page
     @books = Book.page(params[:page])
   end
+
+  def show
+    @book = Book.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => e
+    redirect_to books_path, alert: e.message
+  end
 end
